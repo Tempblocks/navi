75d6044719942f4f557e73170fd98b2e43e6f25f
Tempblocks <86061209+Tempblocks@users.noreply.github.com>
2021-07-17 23:25:19 +0530

    Add files via upload

---
+++ sidenav.js
@@ -0,0 +1,7 @@
+function openNav() {
+                document.getElementById("myul").style.transform = "translateX(0px)";
+       }
+       
+       function closeNav() {
+                document.getElementById("myul").style.transform = "translateX(-300px)";
+       }
